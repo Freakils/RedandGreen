7002fd4df8271243a2e2efc6b6218efdf43e2ad5
luvo <luvo@projectcodex.co>
2015-09-28 13:22:29 +0200

    task3

---
+++ task3.js
@@ -0,0 +1,15 @@
+
+var redbutton = document.getElementById("redbutton");
+redbutton.addEventListener("click", function (){
+	var Block = document.getElementById("Block");
+	Block.className="red"
+});
+
+
+var greenbutton = document.getElementById("greenbutton");
+greenbutton.addEventListener("click", function (){
+	var Block = document.getElementById("Block");
+	Block.className="green"
+});
+
+
