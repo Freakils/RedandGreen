abfdd93f2094e16ceb8578074d6f347c56d07f7d
Freakils <luvo@projectcodex.co>
2015-09-29 09:50:57 +0200

    Update task3.js

--- task3.js
+++ task3.js
@@ -2,14 +2,24 @@
 var redbutton = document.getElementById("redbutton");
 redbutton.addEventListener("click", function (){
 	var Block = document.getElementById("Block");
-	Block.className="red"
+	Block.className="red";
+	
+var TheBlock = document.getElementById("TheBlock");
+    document.getElementById("TheBlock");
+    TheBlock.innerHTML = "Stop";
+    TheBlock.className = "red";
 });
 
 
 var greenbutton = document.getElementById("greenbutton");
 greenbutton.addEventListener("click", function (){
 	var Block = document.getElementById("Block");
-	Block.className="green"
+	Block.className="green";
+
+var TheBlock = document.getElementById("TheBlock");
+	document.getElementById("TheBlock");
+	TheBlock.innerHTML = "Go";
+	TheBlock.className ="green";
 });
 
 
