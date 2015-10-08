c11c07aabaaec168ec71e7bfb696e1e6deaf8538
Freakils <luvo@projectcodex.co>
2015-10-08 14:27:46 +0200

    index-links

--- task3.js
+++ task3.js
@@ -1,15 +1,25 @@
 
 var redbutton = document.getElementById("redbutton");
 redbutton.addEventListener("click", function (){
-	var Block = document.getElementById("Block");
-	Block.className="red"
+	var Fish = document.getElementById("TheBlock");
+	Fish.className="red";
+	
+	var TheBlock = document.getElementById("TheBlock");
+    document.getElementById("TheBlock");
+    TheBlock.innerHTML = "Stop";
+    TheBlock.className = "red";
 });
 
 
 var greenbutton = document.getElementById("greenbutton");
 greenbutton.addEventListener("click", function (){
-	var Block = document.getElementById("Block");
-	Block.className="green"
-});
+	var Block = document.getElementById("TheBlock");
+	Block.className="green";
 
+	var TheBlock = document.getElementById("TheBlock");
+	document.getElementById("TheBlock");
+	TheBlock.innerHTML = "Go";
+	TheBlock.className ="green";
+ 
+});
 
