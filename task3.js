573ff289db26fbc66375cc35cf91f3ab892a418d
Freakils <luvo@projectcodex.co>
2015-10-14 14:46:39 +0200

    a few changes

--- task3.js
+++ task3.js
@@ -1,24 +1,24 @@
 var redbutton = document.getElementById("redbutton");
-redbutton.addEventListener("click", function (){
-	var Fish = document.getElementById("TheBlock");
-	Fish.className="red";
+		redbutton.addEventListener("click", function (){
+			var Fish = document.getElementById("TheBlock");
+				Fish.className="red";
 	
-	var TheBlock = document.getElementById("TheBlock");
-    document.getElementById("TheBlock");
-    TheBlock.innerHTML = "Stop";
-    TheBlock.className = "red";
+		var TheBlock = document.getElementById("TheBlock");
+    		document.getElementById("TheBlock");
+    			TheBlock.innerHTML = "Stop";
+    			TheBlock.className = "red";
 });
 
 
 var greenbutton = document.getElementById("greenbutton");
-greenbutton.addEventListener("click", function (){
-	var Block = document.getElementById("TheBlock");
-	Block.className="green";
+		greenbutton.addEventListener("click", function (){
+			var Block = document.getElementById("TheBlock");
+				Block.className="green";
 
-	var TheBlock = document.getElementById("TheBlock");
-	document.getElementById("TheBlock");
-	TheBlock.innerHTML = "Go";
-	TheBlock.className ="green";
+		var TheBlock = document.getElementById("TheBlock");
+			document.getElementById("TheBlock");
+				TheBlock.innerHTML = "Go";
+				TheBlock.className ="green";
  
 });
 
